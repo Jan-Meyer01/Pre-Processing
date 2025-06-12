b4ea8fcaed23e303a005001eb797c6522047436c
Jan-Meyer01 <jan01.meyer@uni-luebeck.de>
2025-06-12 10:44:38 +0200

    Fixed saving of phantoms.

--- phantoms_jemris_travelling_head.py
+++ phantoms_jemris_travelling_head.py
@@ -9,7 +9,7 @@ from scipy.io import savemat
 import h5py
 
 # custom imports
-from utils import is_image_file
+from utils import is_image_file, save_HDF5
 
 '''
 Script for turning the MPM and T2 data from the travelling head study into phantoms for JEMRIS.
@@ -18,9 +18,9 @@ You should either choose Tmaps+mat for use in the GUI or Rmaps+h5 for a script.
 
 # parse in and out folders through the command line
 p = argparse.ArgumentParser(description='Script for turning the MPM and T2 data from the travelling head study into phantoms for JEMRIS.')
-p.add_argument('-i', '--input_dir', type=str, default="./processed_data/travelling_head", help='input directory where all the data is stored in a BIDS file structure.') # required=True,
+p.add_argument('-i', '--input_dir', type=str, default="./processed_data/travelling_head", help='input directory where all the data is stored in a BIDS file structure.') 
+p.add_argument('-f', '--format', type=str, default="mat", help='Choose the files format in which the phantoms will be saved: mat, h5.')
 p.add_argument('-s', '--slice', action='store_true', default=False, help='Use this argument when you want to get slices instead of volumes.') 
-p.add_argument('-f', '--format', type=str, default="mat", help='Choose the files format in which the phantoms will be saved: mat, h5.') # required=True,
 p.add_argument('-R', '--Rmaps', action='store_true', default=False, help='Use this argument when you want to use R maps instead of T maps.')  
 args = p.parse_args()
 
@@ -41,7 +41,7 @@ sites = [f.name for f in scandir(input_dir)]
 for site in sites:
     print('  working on site:     ', site)
     # get subjects
-    subjects = ['sub-phy001'] #[f.name for f in scandir(join(input_dir, site))]
+    subjects = [f.name for f in scandir(join(input_dir, site))]
     
     for subject in subjects:
         print('    analyzing subject: ', subject)
@@ -55,7 +55,7 @@ for site in sites:
             files = [f.path for f in scandir(input_path)]
             
             # define target folder for phantoms to go to
-            target_folder = join(input_path,'phantoms')
+            target_folder = join(input_path,'phantoms_jemris')
             # if the folder does not yet exist create it
             if not isdir(target_folder):
                 makedirs(target_folder)
@@ -73,10 +73,12 @@ for site in sites:
             for file in files:
                 # if the file is a nifti image
                 if is_image_file(file):
-                    # load PD, R1, R2* or T2 and correct for ms as required by JEMRIS
+                    # load MPMs + T2 and correct for ms as required by JEMRIS
                     if file.endswith('PD.nii'):
                         PD_image  = nib.load(file).get_fdata()
                         PD_file = file
+                        # rescale PD image to intensity range [0,1]
+                        PD_image = PD_image/np.max(np.max(PD_image))
                     elif file.endswith('R1.nii'):
                         R1_image  = nib.load(file).get_fdata()/1000
                     elif file.endswith('T1.nii'):
@@ -91,7 +93,7 @@ for site in sites:
                         R2_image  = nib.load(file).get_fdata()/1000
             
             # check whether the images exist and have the same size
-            assert type(PD_image)     != type(None), 'No PD image found in {} !!'.format(input_path)
+            assert type(PD_image) != type(None), 'No PD image found in {} !!'.format(input_path)
             if args.Rmaps:
                 print('')
                 assert type(R1_image)     != type(None), 'No R1 image found in {} !!'.format(input_path)
@@ -117,47 +119,52 @@ for site in sites:
             if args.format == 'mat':
                 # if slices are selected
                 if args.slice:
-                    # save all slices .mat files
-                    save_name = '{0}_{1}_phantom_Slice{2:03d}.mat'.format(subject,session,slice_num+1)
                     for slice_num in range(PD_image.shape[2]):
+                        # save all slices .mat files
+                        save_name = '{0}_{1}_phantom_jemris_slice{2:03d}.mat'.format(subject,session,slice_num+1)
                         # if specified add R maps else use T maps
                         if args.Rmaps:
                             data = {'M0':PD_image[:,:,slice_num],'R1':R1_image[:,:,slice_num],'R2s':R2star_image[:,:,slice_num],'R2':R2_image[:,:,slice_num],'DB':db[:,:,slice_num],'OFFSET':offset,'RES':RES,'Shape':[PD_image.shape[0],PD_image.shape[1]]}
                         else:
                             data = {'M0':PD_image[:,:,slice_num],'T1':T1_image[:,:,slice_num],'T2s':T2star_image[:,:,slice_num],'T2':T2_image[:,:,slice_num],'DB':db[:,:,slice_num],'OFFSET':offset,'RES':RES,'Shape':[PD_image.shape[0],PD_image.shape[1]]}
+                        # save .mat files to target folder
+                        savemat(join(target_folder, save_name), data)
                 # if there is nothing specified
                 else:
                     # save the volumes to a single .mat file
-                    save_name = '{0}_{1}_phantom_Vol.mat'.format(subject,session)
+                    save_name = '{0}_{1}_phantom_jemris_vol.mat'.format(subject,session)
                     # if specified add R maps else use T maps
                     if args.Rmaps:
                         data = {'M0':PD_image,'R1':R1_image,'R2s':R2star_image,'R2':R2_image,'DB':db,'OFFSET':offset,'RES':RES,'Shape':[PD_image.shape[0],PD_image.shape[1],PD_image.shape[2]]}
                     else:
                         data = {'M0':PD_image,'T1':T1_image,'T2s':T2star_image,'T2':T2_image,'DB':db,'OFFSET':offset,'RES':RES,'Shape':[PD_image.shape[0],PD_image.shape[1],PD_image.shape[2]]}
-                # save .mat file to target folder
-                savemat(join(target_folder, save_name), data)
+                    # save .mat file to target folder
+                    savemat(join(target_folder, save_name), data)
             elif args.format == 'h5':
                 # if slices are selected
                 if args.slice:
-                    # save all slices as .h5 files
-                    save_name = '{0}_{1}_phantom_Slice{2:03d}.h5'.format(subject,session,slice_num+1)
-                    # format data for the phantom
-                    data = np.zeros((PD_image.shape[1],PD_image.shape[0],5))            # first init data
-                    data[:,:,0] = PD_image[:,:,slice_num].transpose()             # add M0 (PD)  
-                    data[:,:,4] = db[:,:,slice_num].transpose()             # add DB (chemical shift --> set to zero)                    
-                    # if specified add R maps else use T maps
-                    if args.Rmaps:
-                        data[:,:,1] = R1_image[:,:,slice_num].transpose()         # add R1
-                        data[:,:,2] = R2_image[:,:,slice_num].transpose()         # add R2
-                        data[:,:,3] = R2star_image[:,:,slice_num].transpose()     # add R2*  
-                    else:
-                        data[:,:,1] = T1_image[:,:,slice_num].transpose()         # add T1
-                        data[:,:,2] = T2_image[:,:,slice_num].transpose()         # add T2
-                        data[:,:,3] = T2star_image[:,:,slice_num].transpose()     # add T2*  
+                    for slice_num in range(PD_image.shape[2]):
+                        # save all slices as .h5 files
+                        save_name = '{0}_{1}_phantom_jemris_slice{2:03d}.h5'.format(subject,session,slice_num+1)
+                        # format data for the phantom
+                        data = np.zeros((PD_image.shape[1],PD_image.shape[0],5))            # first init data
+                        data[:,:,0] = PD_image[:,:,slice_num].transpose()             # add M0 (PD)  
+                        data[:,:,4] = db[:,:,slice_num].transpose()             # add DB (chemical shift --> set to zero)                    
+                        # if specified add R maps else use T maps
+                        if args.Rmaps:
+                            data[:,:,1] = R1_image[:,:,slice_num].transpose()         # add R1
+                            data[:,:,2] = R2_image[:,:,slice_num].transpose()         # add R2
+                            data[:,:,3] = R2star_image[:,:,slice_num].transpose()     # add R2*  
+                        else:
+                            data[:,:,1] = T1_image[:,:,slice_num].transpose()         # add T1
+                            data[:,:,2] = T2_image[:,:,slice_num].transpose()         # add T2
+                            data[:,:,3] = T2star_image[:,:,slice_num].transpose()     # add T2* 
+                        # create groups and datasets structure for JEMRIS phantom
+                        save_HDF5(save_dir=join(target_folder, save_name), data=data, offset=offset, resolution=RESOS)      
                 # if there is nothing specified
                 else:
                     # save the volumes to a single .h5 file
-                    save_name = '{0}_{1}_phantom_Vol.h5'.format(subject,session)
+                    save_name = '{0}_{1}_phantom_jemris_vol.h5'.format(subject,session)
                     # format data for the phantom
                     data = np.zeros((PD_image.shape[2],PD_image.shape[1],PD_image.shape[0],5))            # first init data
                     data[:,:,:,0] = PD_image.transpose()             # add M0 (PD)  
@@ -172,12 +179,7 @@ for site in sites:
                         data[:,:,:,2] = T2_image.transpose()         # add T2
                         data[:,:,:,3] = T2star_image.transpose()     # add T2*  
                 
-                # create groups and datasets structure for JEMRIS phantom
-                f = h5py.File(join(target_folder, save_name), "w")
-                f.require_group(name='sample')
-                f.create_dataset(name='sample/data', data=data)
-                f.create_dataset(name='sample/offset', data=offset)
-                f.create_dataset(name='sample/resolution', data=RESOS)
-                f.close()
+                    # create groups and datasets structure for JEMRIS phantom
+                    save_HDF5(save_dir=join(target_folder, save_name), data=data, offset=offset, resolution=RESOS)
         
 #print('Finished processing at ', time.strftime("%H:%M:%S", time.localtime()))  
